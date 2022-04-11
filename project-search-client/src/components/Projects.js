a0fbc75e2b34b170af49759c2014fb865a44f884
canosaer <67963828+canosaer@users.noreply.github.com>
2022-04-10 20:41:00 -0400

    1.0

--- project-search-client/src/components/Projects.js
+++ project-search-client/src/components/Projects.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState, useEffect } from 'react';
-import projectData from '../store/projectData'
 import { Context } from '../store/store'
 import Filters from './Filters'
 import axios from 'axios';
@@ -7,17 +6,20 @@ import axios from 'axios';
 export default function Projects() {
     const [state, dispatch] = useContext(Context)
     const [projects, setProjects] = useState([])
-    const [searchTerm, setSearchTerm] = useState([])
 
     const getProjects = async () => {
 
         let request = `http://localhost:1337/${state.activeTags[0]}`
 
         try {
-        const response = await axios.get(request)
-        setProjects(response.data)
+            const response = await axios.get(request)
+            let filteredProjects = []
+            if(state.searchTerm){
+                setProjects(response.data.filter(project => project.name.toLowerCase().includes(state.searchTerm.toLowerCase())))
+            }
+            else setProjects(response.data)
         } catch (err) {
-        console.log(err.message, err.code)
+            console.log(err.message, err.code)
         }
     }
 
@@ -32,20 +34,18 @@ export default function Projects() {
             <div className="projects__card-display">
                 {projects.map((project, i) => {
                     const key = `project--${i}`
+   
+                    return(
+                        <article className="projects__card" key={key}>
+                            <a className="projects__subheading" href={project.url} target="_blank">{project.name}</a>
+                            <p className="projects__caption">{project.desc}</p>
+                            <div className="projects__link-row">
+                                <a className="projects__link" href={project.url} target="_blank">View Site</a>
+                                <a className="projects__link" href={project.repo} target="_blank">View Code</a>
+                            </div>
+                        </article>
+                    )
 
-                    
-                        return(
-                            <article className="projects__card" key={key}>
-                                <a className="projects__subheading" href={project.url} target="_blank">{project.name}</a>
-                                <p className="projects__caption">{project.desc}</p>
-                                <div className="projects__link-row">
-                                    <a className="projects__link" href={project.url} target="_blank">View Site</a>
-                                    <a className="projects__link" href={project.repo} target="_blank">View Code</a>
-                                </div>
-                            </article>
-                        )
-
-                    
                 })}
             </div>
         </section>
